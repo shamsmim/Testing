f32776486dc13e4d99cda1d73b70fc70b5ecb908
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-08-07 23:30:03 +0600

    Add files via upload

---
+++ Array Library Methods (28) part-2/Index.js
@@ -0,0 +1,10 @@
+// array methods
+// shift(), unshift(), splice(pos,neo,ele1,ele2..), sp..
+
+var names = ["Anis", "Islam", "Shams"];
+
+console.log(names);
+
+  Adding elements using splice 
+  names.splice(2,0,"Karim","Rahim");
+  console.log(names);
