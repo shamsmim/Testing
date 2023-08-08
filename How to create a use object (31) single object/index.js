d6a961fc658cfe53e847ca6784afd776375d234f
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-08-08 23:33:21 +0600

    Add files via upload

---
+++ How to create a use object (31) single object/index.js
@@ -0,0 +1,17 @@
+//How to create onjecct
+
+var name = "Ash Shams Mim";
+var age = 26;
+var cgpa = 3.93;
+var lang = ["Bengali", "Hindi", "English"];
+
+// console.log(age);
+
+var student1 = {
+    name : "Ash Shams Mim",
+    age : 27,
+    cgpa : 3.93,
+    lang : ["Bengali", "Hindi", "English"]
+}
+
+console.log(student1.age);
