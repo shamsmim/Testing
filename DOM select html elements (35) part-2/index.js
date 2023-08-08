86a199859f2b588aa4c16cc53c6ce61502455a44
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-08-08 23:34:45 +0600

    Add files via upload

---
+++ DOM select html elements (35) part-2/index.js
@@ -0,0 +1,10 @@
+// getElementsByTagName 
+
+// var myheading = document.getElementById("heading1");
+// myheading.innerHTML = "Hello"
+
+// document.getElementById("para").innerHTML = "Bye ";
+
+// var myheading2 = document.getElementById("heading2")
+
+// myheading2.innerHTML = "Good Bye Good Bye";
