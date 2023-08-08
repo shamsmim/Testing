9cdc35d5071deaeff3cb71d4a10b3e1bed237b6e
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-08-08 23:34:10 +0600

    Add files via upload

---
+++ Date object and date methods (34)/DATE_OBJECT.js
@@ -0,0 +1,14 @@
+var date = new Date();
+console.log(date);
+
+var year = date.getFullYear();
+console.log(year);
+
+var month = date.getMonth();
+console.log(month);
+
+var currentDay = date.getDay();
+console.log(currentDay);
+
+var currentHour = date.getHours();
+console.log(currentHour);
