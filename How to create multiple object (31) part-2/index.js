75c509180fab84edeeff2ea234d83afd66ead2b8
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-08-08 23:33:34 +0600

    Add files via upload

---
+++ How to create multiple object (31) part-2/index.js
@@ -0,0 +1,33 @@
+//How to create an object
+// How to print the value of an object
+// Adding a Constractor 
+
+var name = "Ash Shams Mim";
+var age = 26;
+var cgpa = 3.93;
+var lang = ["Bengali", "Hindi", "English"];
+
+// console.log(age);
+
+var student1 = {
+    name : "Rakib Islam",
+    age : 27,
+    cgpa : 3.33,
+    lang : ["Bengali", "Nepali", "English"]
+}
+
+var student2 = {
+    name : "Tahmid Islam",
+    age : 27,
+    cgpa : 3.13,
+    lang : ["Bengali", "Turkish", "English"]
+}
+
+var student3  = {
+    name : "Ash Shams Mim",
+    age : 27,
+    cgpa : 3.93,
+    lang : ["Bengali", "Hindi", "English"]
+}
+
+console.log(student2.lang);
