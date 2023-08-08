96dd9269733f35bbbc9edbe10dfd237c1ada3afe
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-08-08 23:34:28 +0600

    Add files via upload

---
+++ DOM select html elements (35)/index.js
@@ -0,0 +1,12 @@
+// Get element by ID
+
+
+var myheading = document.getElementById("heading1");
+myheading.innerHTML = "Hello"
+
+document.getElementById("para").innerHTML = "Bye ";
+
+var myheading2 = document.getElementById("heading2")
+
+myheading2.innerHTML = "Good Bye Good Bye";
+
